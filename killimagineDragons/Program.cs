e3e07f700a1bf20576c4fd3541669ae63414e08c
RichFisher <rich.fishernc@gmail.com>
2019-03-12 20:01:41 -0500

    i dont think nathan likes that band

--- killimagineDragons/Program.cs
+++ killimagineDragons/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using killimagineDragons.Songs;
 
 namespace killimagineDragons
 {
@@ -6,7 +9,29 @@ namespace killimagineDragons
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var goodSongs = new List<Song>();
+            var allSongs = new List<Song>();
+            allSongs.Add(new Song("Bleeding Out", "Imagine Dragons"));
+            allSongs.Add(new Song("Whatever It Takes", "Imagine Dragons"));
+            allSongs.Add(new Song("Friction", "Imagine Dragons"));
+            allSongs.Add(new Song("Song4", "Artist2"));
+            allSongs.Add(new Song("Song5", "Artist2"));
+            allSongs.Add(new Song("Song6", "Artist3"));
+            allSongs.Add(new Song("Song7", "Artist3"));
+            allSongs.Add(new Song("Song8", "Artist2"));
+            allSongs.Add(new Song("Song9", "Artist4"));
+            allSongs.Add(new Song("Song10", "Artist3"));
+            allSongs.Add(new Song("Song11", "Artist5"));
+            allSongs.Add(new Song("Amsterdam", "Imagine Dragons"));
+
+            goodSongs = allSongs.Where(song => song.Artist != "Imagine Dragons").ToList();
+
+            foreach(Song song in goodSongs)
+            {
+                Console.WriteLine($"{song.Name}: {song.Artist}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
---
+++ killimagineDragons/Song.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace killimagineDragons.Songs
+{
+    class Song
+    {
+        public string Name { get; set; }
+        public string Artist { get; set; }
+
+        public Song(string name, string artist)
+        {
+            Name = name;
+            Artist = artist;
+        }
+    }
+}
