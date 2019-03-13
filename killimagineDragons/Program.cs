18bcf8d9e807157e6a61e69413bc74bebba9604f
RichFisher <rich.fishernc@gmail.com>
2019-03-12 19:34:55 -0500

    Add project files.

---
+++ killimagineDragons/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace killimagineDragons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
